3198d45117b9890a39bc62d1c00f729f12b0567f
Pablo Daza <aketza.daza@me.com>
2020-12-11 15:07:52 -0400

    flume

--- bigdata-cluster/config/get-telegram-updates.sh
+++ bigdata-cluster/config/get-telegram-updates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir /TelegramUpdates
 while true; do
-  wget -O ./TelegramUpdates/updates https://api.telegram.org/bot1498998529:AAG7I6cAZr1e7Zsd5e-EQES7QfwfvLnn_Vg/getUpdates
+  wget -O /TelegramUpdates/updates https://api.telegram.org/bot1498998529:AAG7I6cAZr1e7Zsd5e-EQES7QfwfvLnn_Vg/getUpdates
   sleep 5;
 done
